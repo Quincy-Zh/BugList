d784c8a12c776188951dfefe92075f7f33c6133f
Quincy W <quincy@owncloud.lan>
2016-12-10 22:03:27 +0800

    完善首页展示功能

--- app/__init__.py
+++ app/__init__.py
@@ -16,33 +16,28 @@ login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
-    
     app.config.from_object(config[config_name])
-    
     config[config_name].init_app(app)
-    
     bootstrap.init_app(app)
-    
-    if app.config.has_key('USER_DEF_CDN_BOOTSTRAP'):
+
+    if 'USER_DEF_CDN_BOOTSTRAP' in app.config:
         cdn_bootstrap = WebCDN(app.config['USER_DEF_CDN_BOOTSTRAP'])
         app.extensions['bootstrap']['cdns']['bootstrap'] = cdn_bootstrap
-        
-        
-    if app.config.has_key('USER_DEF_CDN_JQUERY'):
+
+    if 'USER_DEF_CDN_JQUERY' in app.config:
         cdn_jquery = WebCDN(app.config['USER_DEF_CDN_JQUERY'])
         app.extensions['bootstrap']['cdns']['jquery'] = cdn_jquery
-    
     db.init_app(app)
     login_manager.init_app(app)
-    
+
     # 附加路由和自定义的错误页面
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-    
+
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
-    
+
     from .api import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api')
-    
+
     return app
--- app/api/views.py
+++ app/api/views.py
@@ -7,20 +7,17 @@ import random
 
 from flask import current_app, request, redirect, url_for, make_response, jsonify
 from . import api
-#from .. import db
 from ..models import User, Bug, Record, \
     Product, Comment, Source, Progress, \
     Permission, SystemSettings, Role
-from ..decorators import admin_required, permission_required 
-#from .forms import BugForm, BatchForm, FilterForm, CommentForm
-
+from ..decorators import admin_required, permission_required
 from flask_login import login_required, current_user
 
-    
+
 @api.route('/list/<title>')
 def list(title):
     items = None
-    
+
     if title.lower() == 'product':
         keyword = request.args.get('term', '')
         products = Product.query.filter(Product.title.like(u'%{0}%'.format(keyword))).all()
@@ -37,47 +34,55 @@ def list(title):
             product_id = int(request.args.get('product'))
         except:
             product_id = -1
-            
+
         try:
             progress_id = int(request.args.get('progress'))
         except:
             progress_id = -1
-            
+
         try:
             page = int(request.args.get('page'))
         except:
             page = 1
-            
+
+        try:
+            source_id = int(request.args.get('source'))
+        except:
+            source_id = -1
+
         if product_id != -1:
             query = Bug.query.filter(Bug.product_id==product_id).order_by(Bug.id.desc())
         else:
             query = Bug.query.order_by(Bug.id.desc())
-        
+
+        if source_id != -1:
+            query = query.filter(Bug.source_id == source_id)
+
         if progress_id != -1:
             query = query.filter(Bug.progress_id == progress_id)
-            
-        pagination = query.paginate(page, 
+
+        pagination = query.paginate(page,
             per_page=current_app.config['MATERIAL_COUNT_PER_PAGE'],
             error_out=False)
-            
+
         items = [{'id': b.id, 'text': b.text, 'product': b.product.title, 'progress': b.progress.text} for b in pagination.items]
-        
-        result = {'items': items, 
-            'count': len(items),
+        print(pagination.total)
+        print(pagination.has_prev)
+        print(pagination.has_next)
+
+        result = {'items': items,
+            'count': pagination.total,
             'page_size': current_app.config['MATERIAL_COUNT_PER_PAGE'],
             'page': page
             }
     else:
         current_app.logger.debug(u'"{0}" Not Found.'.format(title))
-        
         result = {'errormsg': 'title not found.'}
-        
     response = make_response(jsonify(result))
-    
+
     # 跨域访问
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'POST'
     response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type' 
 
     return response
-    
